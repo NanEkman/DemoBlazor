4284a7253fa1fc616a66775c81062856ddedafee
NanEkman <nen_92@hotmail.com>
2025-03-13 20:06:54 +0100

    added mocked data for two predefined lists in mypage

--- Model/Product.cs
+++ Model/Product.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public decimal Amount { get; set; }
         public string Category { get; set; }
+
+        public string ShoppingListName { get; set; }
     }
 }
