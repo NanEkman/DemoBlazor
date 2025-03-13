6026ed802824958937b6c721beebadb86721b761
NanEkman <nen_92@hotmail.com>
2025-03-13 18:18:58 +0100

    created Models ShoppingList and Product

---
+++ Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoBlazor.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Model/ShoppingList.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoBlazor.Model
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
